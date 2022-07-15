cf2e29550c87c3e113a9bd63c2139d7c9bcfe8a9
Александр Боровик <aoborovik.sbt@sberbank.ru>
2022-07-15 16:14:14 +0600

    fix import

--- src/pages/MatchPage.tsx
+++ src/pages/MatchPage.tsx
@@ -1,4 +1,4 @@
-import React, { memo } from "react";
+import React from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import { Layout, Result, Button, PageHeader } from "antd";
 import { FrownOutlined } from '@ant-design/icons';
