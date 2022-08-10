67813ab2fa4c3f673109a0272b9455eca303c391
unsOmen <unsomen0405@gmail.com>
2022-08-10 15:09:27 +0600

    test

--- src/api/background.js
+++ src/api/background.js
@@ -3,7 +3,15 @@ import FaceitService from "./FaceitService";
 
 
 chrome.action.onClicked.addListener(function (activeTab) {
-  chrome.tabs.create({ url: chrome.runtime.getURL('index.html') });
+  try {
+    console.debug("ACTIVE TAB", activeTab);
+    console.debug("ACTIVE TAB2", window.location.toString());
+  } catch (e) {
+    console.error(e);
+  }
+  // chrome.tabs.create({ url: chrome.runtime.getURL('index.html') });
+  ///1-12271de5-1838-40ed-a22b-0129c7a39f39
+  chrome.tabs.create({ url: chrome.runtime.getURL('index.html#/1-12271de5-1838-40ed-a22b-0129c7a39f39') });
 });
 
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
